db5758c92f827c6f2e6d8fb8378708ef7f96af2d
shemishtamesh <shemishtamail@gmail.com>
2023-04-25 18:35:49 +0300

    switch KC_PPLS (numpad plus) with KC_PLUS (regular plus)

--- keyboards/momoka_ergo/keymaps/shemishtamesh/keymap.c
+++ keyboards/momoka_ergo/keymaps/shemishtamesh/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_NUM] = LAYOUT(
         KC_F10,  KC_F11,  KC_F12,  KC_QUES, KC_GRV,  KC_WSTP,                                    KC_LT,   KC_WFAV, KC_SLSH, KC_ASTR, KC_COMM, KC_GT,
-        KC_F7,   KC_F8,   KC_F9,   KC_AMPR, KC_ASTR, KC_GRV,                                     KC_LPRN, KC_7,    KC_8,    KC_9,    KC_PPLS, KC_RPRN,
+        KC_F7,   KC_F8,   KC_F9,   KC_AMPR, KC_ASTR, KC_GRV,                                     KC_LPRN, KC_7,    KC_8,    KC_9,    KC_PLUS, KC_RPRN,
         KC_F4,   KC_F5,   KC_F6,   KC_DLR,  KC_PERC, KC_CIRC,                                    KC_LCBR, KC_4,    KC_5,    KC_6,    KC_MINS, KC_RCBR,
         KC_F1,   KC_F2,   KC_F3,   KC_EXLM, KC_AT,   KC_HASH,                                    KC_LBRC, KC_1,    KC_2,    KC_3,    KC_EQL,  KC_RBRC,
         KC_TILD, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT,                                                      KC_0,    KC_DOT,  KC_COMM, KC_ENT,  KC_TAB,
